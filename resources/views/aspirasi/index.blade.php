1bea1d0fdb5428937c43b993d86c3d370f53c445
mochamadyunusikhsanodin <yunusikhsanodin31@gmail.com>
2023-05-21 21:17:32 +0700

    css tambahann

--- resources/views/aspirasi/index.blade.php
+++ resources/views/aspirasi/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pengajuan</title>
-    <link rel="stylesheet" href="{{ asset('frontend/assets/css/styledashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/styledashboard1.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.0.0/fonts/remixicon.css" rel="stylesheet">
 </head>
 <body>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Daftar Pegawai</title>
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/styledashboard.css') }}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/styledashboard2.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/remixicon@3.0.0/fonts/remixicon.css" rel="stylesheet">
     </head>
     <body>
